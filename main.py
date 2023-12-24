5253fb9de6f9df290c26803bb6e627f54a282d04
Justin Looi <justinlooijw@gmail.com>
2023-12-25 02:23:02 +0800

    Add OpenAI client initialization and update chat endpoint

--- main.py
+++ main.py
@@ -3,29 +3,37 @@ from pydantic import BaseModel
 import openai
 import os
 from dotenv import load_dotenv
+import uvicorn
 
 load_dotenv()  # Load environment variables from .env file
 
+# Initialize OpenAI client
+client = openai.Client(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+# Define Pydantic model for chat message
 class ChatMessage(BaseModel):
     message: str
 
 app = FastAPI()
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print(os.getenv("OPENAI_API_KEY"))
-
+# Define the chat endpoint
 @app.post("/chat")
 async def chat(chat_message: ChatMessage):
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+        chat_completion = client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
-                    "content": chat_message.message
+                    "content": chat_message.message,
                 }
-            ]
+            ],
+            model="gpt-3.5-turbo",
         )
-        return {"response": response.choices[0].message["content"]}
+        response_content = chat_completion.choices[0].message.content
+        return {"response": response_content}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
