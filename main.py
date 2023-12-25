8b64d8666107cd438f1c9d19357ae83610d8f1ca
Justin Looi <justinlooijw@gmail.com>
2023-12-26 00:57:22 +0800

    Add chat-stream endpoint for streaming responses

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException,Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 import openai
 import os
@@ -47,4 +48,35 @@ async def chat(chat_message: ChatMessage):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# @app.post("/chat-stream")
+# async def chat(chat_message: ChatMessage):
+#     try:
+#         stream = client.chat.completions.create(
+#             model="gpt-3.5-turbo",
+#             messages=[{"role": "user", "content": chat_message.message}],
+#             stream=True,
+#         )
+#         for chunk in stream:
+#             print(chunk.choices[0].delta.content or "", end="\n")
+#     except Exception as e:
+#         print(e)
+
+#     # return Response(event_generator(), media_type="text/event-stream")
+    
+@app.post("/chat-stream")
+async def chat(chat_message: ChatMessage):
+    async def event_generator():
+        try:
+            stream = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": chat_message.message}],
+                stream=True,
+            )
+            for chunk in stream:
+                if chunk.choices[0].delta.content:
+                    yield f"data: {chunk.choices[0].delta.content}\n\n"
+        except Exception as e:
+            yield f"data: Error: {str(e)}\n\n"
+
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
 
